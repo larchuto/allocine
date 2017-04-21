c8ded23d6bb852a9625ce9f5dd3583be36ba1134
larchuto <johan.lafon@phelma.grenoble-inp.fr>
2017-04-21 03:56:13 +0200

    works with python 3

--- allocine.py
+++ allocine.py
@@ -5,9 +5,9 @@ import sys
 import time
 import base64
 import hashlib
-import urllib
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
 import json
-
 
 class allocineRequest():
     def __init__(self):
@@ -24,23 +24,22 @@ class allocineRequest():
        
     def url(self):
         self.tokens['sed']     = time.strftime('%Y%m%d', time.localtime())
-        self.tokens['sig']     = base64.b64encode(hashlib.sha1(self.secretKey + urllib.urlencode(self.ksort(self.tokens))).digest())
+        param_str              = self.secretKey + urlencode(self.ksort(self.tokens))
+        self.tokens['sig']     = base64.b64encode(hashlib.sha1(param_str.encode()).digest())
       
-        return 'http://api.allocine.fr/rest/v3/' + self.route + '?' + urllib.urlencode(self.ksort(self.tokens))
+        return 'http://api.allocine.fr/rest/v3/' + self.route + '?' + urlencode(self.ksort(self.tokens))
 
     def send(self):
-        opener = urllib.URLopener()
-        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
-        u = self.url()
-        print u
+        request = Request(self.url())
+        request.add_header('User-agent','Mozilla/5.0')
         link = None
         # Sometimes the url open can fail.
         while link == None:
             try:
-                link = opener.open(u)
+                link = urlopen(request)
             except IOError:
-                print 'oo...an error'
-        return link.read()
+                print('oo...an error')
+        return link.read().decode()
     
     def found(self):
         return (self.result != '')
@@ -93,7 +92,7 @@ class allocineMovie(allocineRequest):
 
 
 def usage():
-    print '''
+    print('''
 allocine.py title
 =================
 
@@ -102,7 +101,7 @@ allocine.py title
 
     - title : Title or part
 
-    '''
+    ''')
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -111,27 +110,27 @@ if __name__ == '__main__':
         s = allocineSearch(sys.argv[1])
         if s.found():    
             for movie in s.result['feed']['movie']:
-                print '%s - %d' % (movie['originalTitle'], movie['code'])
+                print('%s - %d' % (movie['originalTitle'], movie['code']))
 
         m = allocineMovie(s.result['feed']['movie'][0]['code'])
             
-        print '\n'
-        print 'Genre : '
-        print m.result['movie']['genre']
-        print 'Synopsis : '
-        print m.result['movie']['synopsis']
-        print 'Realisateur'
-        print m.result['movie']['castingShort']['directors']
-        print 'Acteurs'
+        print('\n')
+        print('Genre : ')
+        print(m.result['movie']['genre'])
+        print('Synopsis : ')
+        print(m.result['movie']['synopsis'])
+        print('Realisateur')
+        print(m.result['movie']['castingShort']['directors'])
+        print('Acteurs')
         try:
-            print m.result['movie']['castingShort']['actors']
+            print(m.result['movie']['castingShort']['actors'])
         except:
-            print 'oups'
-        print 'User rating'
-        print m.result['movie']['statistics']['userRating']
+            print('oups')
+        print('User rating')
+        print(m.result['movie']['statistics']['userRating'])
 
-        for k, v in m.result['movie'].iteritems():
-            print k
+        for k, v in m.result['movie'].items():
+            print(k)
 
 
 '''
